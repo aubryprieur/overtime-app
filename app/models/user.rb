69cc947a88436622b58479d799628daa4146b63a
aubry prieur <aubry.prieur@gmail.com>
2020-02-05 16:24:10 +0100

    add change for model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name, :last_name
 end
