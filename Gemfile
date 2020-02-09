68443cc8814969cf6a3ded5725fdaf11a91ae6b9
aubry prieur <aubry.prieur@gmail.com>
2020-02-09 15:32:09 +0100

    Implement bootstrap with webpack

--- Gemfile
+++ Gemfile
@@ -28,7 +28,6 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise', '~> 4.7', '>= 4.7.1'
-gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,13 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+import 'bootstrap'
+import './src/application.scss'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle= "tooltip"]').tooltip()
+  $('[data-toggle= "popover"]').popover()
+
+})
