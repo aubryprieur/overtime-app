790428db155b8c5b211c424e6af29b4828b0e827
aubry prieur <aubry.prieur@gmail.com>
2020-02-10 12:07:53 +0100

    Refactored index page to use Post partial and debugged rspec for creating post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @user = User.create!(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+      login_as(@user, :scope => :user)
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id)
     end
 
     it 'can be created'  do
