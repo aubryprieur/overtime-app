f9a67a69ae21e5f51c6ebab0ce0130db73e46ba2
aubry prieur <aubry.prieur@gmail.com>
2020-02-16 16:59:13 +0100

    Customized search in admin dashboard

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -2,30 +2,27 @@ require "administrate/base_dashboard"
 
 class AdminUserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
-    posts: Field::HasMany,
-    id: Field::Number,
-    email: Field::String,
-    password: Field::String,
-    sign_in_count: Field::Number,
-    current_sign_in_at: Field::DateTime,
-    last_sign_in_at: Field::DateTime,
+    id: Field::Number.with_options(searchable: false),
+    email: Field::String.with_options(searchable: true),
+    password: Field::String.with_options(searchable: false),
+    sign_in_count: Field::Number.with_options(searchable: false),
+    current_sign_in_at: Field::DateTime.with_options(searchable: false),
+    last_sign_in_at: Field::DateTime.with_options(searchable: false),
     current_sign_in_ip: Field::String.with_options(searchable: false),
     last_sign_in_ip: Field::String.with_options(searchable: false),
-    first_name: Field::String,
-    last_name: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
-    type: Field::String,
+    first_name: Field::String.with_options(searchable: false),
+    last_name: Field::String.with_options(searchable: false),
+    created_at: Field::DateTime.with_options(searchable: false),
+    updated_at: Field::DateTime.with_options(searchable: false),
+    type: Field::String.with_options(searchable: false),
   }.freeze
 
   COLLECTION_ATTRIBUTES = [
-    :posts,
     :id,
     :email,
   ].freeze
 
   SHOW_PAGE_ATTRIBUTES = [
-    :posts,
     :id,
     :email,
     :sign_in_count,
@@ -47,4 +44,5 @@ class AdminUserDashboard < Administrate::BaseDashboard
     :last_name,
     :type,
   ].freeze
+
 end
