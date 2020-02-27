98b71fb19ae909784a0fe6b8e0e8ac99139cb554
aubry prieur <aubry.prieur@gmail.com>
2020-02-27 11:23:30 +0100

    Implemented bootstrap badges with status values on index for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+    def status_span_generator status
+      case status
+      when 'submitted'
+        content_tag(:span, status.titleize, class: 'badge badge-primary')
+      when 'approved'
+        content_tag(:span, status.titleize, class: 'badge badge-success')
+      when 'rejected'
+        content_tag(:span, status.titleize, class: 'badge badge-danger')
+      end
+    end
 end
