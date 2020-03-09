8b35633b9889104b3fa8a4489235014f7ebaeef1
aubry prieur <aubry.prieur@gmail.com>
2020-03-09 11:27:24 +0100

    Integrated application helper for status labels and integrated enums for audit log

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,25 @@ module ApplicationHelper
   def active?(path)
     "active" if current_page?(path)
   end
+
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+    def status_span_generator status
+      case status
+      when 'submitted'
+        content_tag(:span, status.titleize, class: 'badge badge-primary')
+      when 'approved'
+        content_tag(:span, status.titleize, class: 'badge badge-success')
+      when 'rejected'
+        content_tag(:span, status.titleize, class: 'badge badge-danger')
+      when 'pending'
+        content_tag(:span, status.titleize, class: 'badge badge-primary')
+      when 'confirmed'
+        content_tag(:span, status.titleize, class: 'badge badge-success')
+      end
+    end
 end
