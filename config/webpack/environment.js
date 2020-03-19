10b16eecf4a0a4a3b9bccc272e0f9c1d970b4450
aubry prieur <aubry.prieur@gmail.com>
2020-03-19 16:07:36 +0100

    test some changes with webpacker environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,18 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }))
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+// const webpack = require('webpack')
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery'
+//   })
+// )
 
 module.exports = environment
