7135cb95ce79adfaea1ae90bf4b01e64e1c63d56
aubry prieur <aubry.prieur@gmail.com>
2020-03-19 16:36:13 +0100

    changed environment.js with lewagon setup

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
+const webpack = require('webpack');
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
 environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
   })
-)
+);
 
 module.exports = environment
