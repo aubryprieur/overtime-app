3c5e881b7513db54a9017cdd00a580e2477f704f
aubry prieur <aubry.prieur@gmail.com>
2020-03-20 12:07:11 +0100

    Implemented nil guard for callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,6 @@ class Post < ApplicationRecord
 
     def update_audit_log
       audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
-      audit_log.confirmed!
+      audit_log.confirmed! if audit_log
     end
 end
